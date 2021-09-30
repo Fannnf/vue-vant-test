73fb77aa9018358cb6bcb374a92d1ff51ae52b8a
Justin <2541083586@qq.com>
2021-09-30 12:31:02 +0800

    vue-vant-test1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import Vant from 'vant';
+import 'vant/lib/index.css';
+createApp(App).use(Vant).mount('#app');
+
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
+
+// const app = createApp();
+// app.use(Vant);
+
